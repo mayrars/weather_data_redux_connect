994fa6325c1e96874dfd7602a6ce288b58cb148a
Mayra <mayra203@gmail.com>
2021-10-13 11:13:52 -0500

    Agregando reducer de la ciudad y estado inicial

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
-
 import { createStore } from 'redux';
-
-export const store = createStore(()=>{ }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import { city } from './../reducers/city'
+const initialState = {
+    city: 'Mexico,mx'
+}
+export const store = createStore(city, initialState, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
