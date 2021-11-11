6d070628d433375b807d0c48ddc3e6287ecced25
Mayra <mayra203@gmail.com>
2021-11-11 14:04:35 -0600

    ajuste de las horas

--- src/services/transformForecast.js
+++ src/services/transformForecast.js
@@ -4,9 +4,9 @@ import transformWeather from './transformWeather';
 
 const transformForecast = data => (
     data.list.filter(item => (
-        moment.unix(item.dt).hour() === 6 ||
-        moment.unix(item.dt).hour() === 12 ||
-        moment.unix(item.dt).hour() === 18
+        moment.unix(item.dt).hour() === 13 ||
+        moment.unix(item.dt).hour() === 16 ||
+        moment.unix(item.dt).hour() === 22
     )).map(item => (
         {
             weekDay: moment.unix(item.dt).format('ddd'),
